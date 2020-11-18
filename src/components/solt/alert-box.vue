96bc0944903c111e7cb764fd66d92e3b568b6951
hansongsong <hansongsong@zongheng.com>
2020-11-18 18:02:51 +0800

    2

---
+++ src/components/solt/alert-box.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="alert-box">
+        alert-box
+        <strong>slot的使用</strong>
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="less" scoped>
+#alert-box {
+    font-size: 30px;
+    color: #bbb;
+}
+</style>
+<script>
+export default {
+    name: "alert-box",
+    props: {
+        msg: String,
+    },
+};
+</script>
--- src/pages/views/Vuex.vue
+++ src/pages/views/Vuex.vue
@@ -1,29 +1,35 @@
 <template>
-    <div id="uvex1">uvex1
-    <!--不清楚为啥放在句首就不能使用-->
-    <!-- {{ this.$store.state.count }} -->
-    {{ countss }}
-       
+    <div id="uvex1">
+        uvex1
+        <!--不清楚为啥放在句首就不能使用-->
+        <!-- {{ this.$store.state.count }} -->
+        {{ countss }}
+
         {{ this.$store.state.count }}
-        
+
         {{ this.$store.getters.getCount }}
         <button @click="add">+</button>
         <button @click="del">-</button>
+        <alertbox> 用了插槽这句话就不用省略了 </alertbox>
     </div>
 </template>
 
 <style lang="less" scoped>
-#uvex1{
+#uvex1 {
     font-size: 30px;
     color: #bbb;
 }
 </style>
 <script>
-import {mapState, mapGetters, mapActions} from "vuex"
+import alertbox from "../../components/solt/alert-box.vue";
+import { mapState, mapGetters, mapActions } from "vuex";
 export default {
     name: "vuex1",
     props: {
         msg: String,
+    },
+    components: {
+        alertbox,
     },
     // data() {
     //     return{
@@ -34,43 +40,37 @@ export default {
     data() {
         return {
             countss: this.$store.state.count,
-
         };
     },
-    methods:{
-        add(){
+    methods: {
+        add() {
             //mutation
             // this.$store.commit('addCount',1)
             //Actions
-            this.$store.dispatch('getAddCount',1)
+            this.$store.dispatch("getAddCount", 1);
         },
-        del(){
+        del() {
             //mutation
             // this.$store.commit('delCount',1)
             //Actions
-            this.$store.dispatch('getDelCount',1)
+            this.$store.dispatch("getDelCount", 1);
         },
-        sss(){
-            console.log(`1111111-----this.$store.state.count`+this.$store.state.count)
+        sss() {
+            console.log(
+                `1111111-----this.$store.state.count` + this.$store.state.count
+            );
         },
-
     },
-    created(){
-            // 获取tree结构
-            this.sss()
-
-        },
-    computed:{
-
+    created() {
+        // 获取tree结构
+        this.sss();
+    },
+    computed: {
         //通过配合 ... 拓展符号
         ...mapState({
-            count1:state=>state.count
+            count1: (state) => state.count,
         }),
-        ...mapGetters([
-            'getCount',
-            'isShow'
-        ]),
-    }
-
+        ...mapGetters(["getCount", "isShow"]),
+    },
 };
 </script>
